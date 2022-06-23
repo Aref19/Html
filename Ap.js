b618da36549411edad276ed97c5a7d0e05ef7998
aref <arefobaid6@gmail.com>
2022-06-23 22:40:11 +0200

    add cat

--- Ap.js
+++ Ap.js
@@ -12,6 +12,7 @@ game.load.image('sky', 'phots/sky.png');
 game.load.image('ground', 'phots/platform.png');
 
 game.load.spritesheet('plyer', 'phots/snjab.png', 48, 48,35);
+game.load.spritesheet('cat', 'phots/cat.png', 48, 48,35);
 }
 
 function create ()
@@ -28,12 +29,19 @@ platforms.enableBody = true;
 var ground = platforms.create(0, game.world.height -30, 'ground');
 ground.scale.setTo(150, 150);
 ground.body.immovable = true;
-//plyer
-player = game.add.sprite(32, game.world.height - 150, 'plyer',20);
+//plyer1
+player = game.add.sprite(200, game.world.height - 150, 'plyer',20);
 game.physics.arcade.enable(player);
 player.body.bounce.y = 0.2;
 player.body.gravity.y = 300;
 player.body.collideWorldBounds = true;
+
+//cat
+cat = game.add.sprite(20, game.world.height-200, 'cat',20);
+game.physics.arcade.enable(cat);
+cat.body.bounce.y = 0.2;
+cat.body.gravity.y = 300;
+cat.body.collideWorldBounds = true;
 
  player.animations.add('left', [15, 16, 17], 10, true);
 player.animations.add('right', [27, 28, 29], 10, true);
@@ -42,14 +50,17 @@ cursors = game.input.keyboard.createCursorKeys();
 //camera
 
 game.camera.height=1000;
-game.camera.width=0;
+game.camera.width=1500;
+
 }
 
 function update ()
 { 
-   console.log("window"+(window. innerWidth-postionRghit));
+    game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN_TIGHT);
+
     
     var hitPlatform = game.physics.arcade.collide(player, platforms);
+    var hitPlatform = game.physics.arcade.collide(cat, platforms);
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
@@ -67,10 +78,9 @@ function update ()
     
         //  Move to the right
         player.body.velocity.x = 100;
-        player.animations.play('right');
-        console.log("widzh" +player.body.velocity.x);
+        player.animations.play('right');    
         postionRghit=2+postionRghit;
-        console.log(postionRghit);
+
     }
     else
     {
@@ -90,11 +100,11 @@ function update ()
         postionRghit=0;
         
     }
-    if((window. innerWidth+postionRghit)>2000){
-        game.camera.unfollow();
-    }
-    
 
+function addtrees(){
+  platformstree=game.add.group();
+
+}
     
 
 }
