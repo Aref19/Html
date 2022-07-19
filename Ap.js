ee3ff9b95b9fc070ba8cbdad0974dc3d891d7cd1
aref <arefobaid6@gmail.com>
2022-07-19 19:04:41 +0200

    update

--- Ap.js
+++ Ap.js
@@ -13,10 +13,11 @@
     var  rightorlink=false ;
     var gewonnen=false;
     var time=0; 
-    var schiss;
+    var schiss=0;
     var player;
+    var phlanze;
     var catgeschossen=false;
-    var game = new Phaser.Game(4000, 1000, Phaser.AUTO,' ' , { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(1800, 1000, Phaser.AUTO,' ' , { preload: preload, create: create, update: update });
 
     //var camera= new Phaser.Camera(game, 1, 32,  10, 20, 20);
     var postionRghit=0;
@@ -28,26 +29,28 @@
     game.load.image('tree', 'phots/tree1.png');
     game.load.image('box', 'phots/box.png');
     game.load.image('term', 'phots/term.jpg');
-
+    game.load.image('phlanz', 'phots/blume.png');
     game.load.image('acorn', 'phots/acorn.png');
+    game.load.image('schiss', 'phots/schiss.png');
     game.load.image('save', 'phots/platform.png');
     game.load.spritesheet('plyer', 'phots/snjab.png', 48, 48,35);
     game.load.spritesheet('cat', 'phots/cat.png', 48, 48,35);
+    game.load.image('wolk', 'phots/wolk.png');
     game.load.audio('hinterground', ['sound/hinterground1.mp3']);
     }
 
     function create ()
 
     {    
-        schiss=upKey = game.input.keyboard.addKey(Phaser.Keyboard.x);
+        
         
     //text
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-  
+ 
     //game    
     game.stage.backgroundColor = "#4488AA";
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.world.setBounds(0, 0, 6000 , 1000);
+    game.world.setBounds(0, 0, 6150 , 1000);
 
    
     
@@ -56,13 +59,16 @@
     platforms = game.add.group();
     platforms.enableBody = true;
     platforms.create(-250, game.world.height -500, 'tree');
-   var save= platforms.create(5800, game.world.height -200, 'save');
+    wolks(platforms);
+   var save= platforms.create(6000, game.world.height -200, 'save');
     save.body.immovable = true;
     creatBox(platforms);
     
     trem= platforms.create(game.world.height+2350, game.world.height -150, 'term');
     trem.body.immovable = true;
-
+    
+   
+    
     var ground = platforms.create(0, game.world.height -30, 'ground');
     ground.scale.setTo(150, 150);
     ground.body.immovable = true;
@@ -95,8 +101,15 @@
     game.camera.width=1500;
 
     //sound
-    hintersound=game.sound.add("hinterground", 2,true);
+    hintersound=game.sound.add("hinterground", 10);
+    hintersound.play();
     acrons = game.add.group();
+    phlanze=game.add.group();
+    phlanze.enableBody=true;
+    creatPhlanz();
+     
+ 
+ 
     acronsschiss = game.add.group();
     //  We will enable physics for any star that is created in this group
     acronsschiss.enableBody=true;
@@ -105,20 +118,22 @@
     //
 
     
-
+  
+ 
     }
 
     function update ()
     { 
-       
+      
    
-    hintersound.play();
+    
         
         if(!gameOver){
             game.physics.arcade.collide(acrons, platforms);
-        
+            game.physics.arcade.collide(phlanze, platforms);
             //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
             game.physics.arcade.overlap(player, acrons,collectStar, null, this);
+            game.physics.arcade.overlap(player, phlanze,takeblum, null, this);
            
             
         game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN_TIGHT);
@@ -148,22 +163,22 @@
             
 
         }
-        if (cursors.left.isDown&&score!=0)
+        if (cursors.left.isDown&&schiss!=0)
         {   
         
             //  Move to the right
           
            player.animations.play('left');  
-            scoreText.x=player.body.x;
-            scoreText.x=player.body.x+20;
-            postionRghit=2+postionRghit;
-            contoll=false;
-          schiss= acronsschiss.create(player.position.x-20, player.position.y+20, 'acorn');
+           scoreText.x=player.body.x;
+           scoreText.x=player.body.x+20;
+           postionRghit=2+postionRghit;
+           contoll=false;
+           var schossenFeuer=acronsschiss.create(player.position.x-20, player.position.y+20, 'schiss');
          
-          schiss.body.gravity.x = -100 ;
-          score=score-1;
+           schossenFeuer.body.gravity.x = -80 ;
+           schiss=schiss-1;
 
-          scoreText.text = 'Score: ' + score;
+
             
             
       //    cronschiss.body.bounce.x = 0.7 + Math.random() * 0.2;
@@ -201,13 +216,7 @@
             }
         }
         
-        /*if( parseInt(player.position.x)== parseInt(cat.position.x)){
-        
-            player.body.bounce.y = 1;
-            player.body.collideWorldBounds = false;
-            gameOver=true;        
-            
-        }*/
+    
        
 
         if(parseInt(cat.position.x)==629){
@@ -276,6 +285,7 @@
     }
 
     function creatBox(platform){
+
     box= platform.create(game.world.height-300, game.world.height -100, 'box');
     box2= platform.create(game.world.height+200, game.world.height -100, 'box');
     box3= platform.create(game.world.height+2000, game.world.height -100, 'box');
@@ -300,6 +310,14 @@
             }
             acorn.body.immovable = true;
         
+    }
+
+    function creatPhlanz(){
+   var p1= phlanze.create(2000, game.world.height -75, 'phlanz');
+   var p2= phlanze.create(3500, game.world.height -75, 'phlanz');
+  
+   p1.body.immovable=true;
+   p2.body.immovable=true;
     }
 
 
@@ -373,9 +391,32 @@ function catzurck(){
 function schiisCat(){
    
     cat.animations.stop();
-    cat.frame = 4; 
     cat.body.collideWorldBounds = false;
     catgeschossen=true;
 
 }
 
+function wolks(plat){
+    var wolk=plat.create(16, 16, 'wolk');
+    var wolk1=plat.create(game.world.height+600, 50, 'wolk');
+    var wolk2=plat.create(game.world.height+1900, 16, 'wolk');
+  
+    var wolk4=plat.create(game.world.height+1500, 16, 'wolk');
+    var wolk6=plat.create(game.world.height+4000, 20, 'wolk');
+    wolk.body.immovable = true;
+    wolk1.body.immovable = true;
+    wolk2.body.immovable = true;
+
+    wolk4.body.immovable = true;
+  
+    wolk6.body.immovable = true;
+
+}
+
+function takeblum(p,c){
+    c.kill();
+   var schiistext= game.add.text(player.body.position.x, 500, ' left drucken Schiss', { fontSize: '32px', fill: '#FF0000' });
+   schiss=1;
+
+  
+}
